00a129f2af2959690a5815cda77d7eeee51e6f04
BeuBen <beuscart.ben@gmail.com>
2024-07-18 16:54:55 +0200

    feat(synchro): fetch all paginated cards at once

--- src/main/java/com/beuben/pokemontcgcollectorbackend/refresh/infrastructure/out/external/adapter/CardAdapter.java
+++ src/main/java/com/beuben/pokemontcgcollectorbackend/refresh/infrastructure/out/external/adapter/CardAdapter.java
@@ -3,10 +3,14 @@ package com.beuben.pokemontcgcollectorbackend.refresh.infrastructure.out.externa
 import com.beuben.pokemontcgcollectorbackend.refresh.application.port.out.provider.CardProvider;
 import com.beuben.pokemontcgcollectorbackend.refresh.domain.card.Card;
 import com.beuben.pokemontcgcollectorbackend.refresh.infrastructure.out.external.api.pokemontcg.PokemonTcgApi;
+import com.beuben.pokemontcgcollectorbackend.refresh.infrastructure.out.external.dto.PokemonTcgCardWrapperDTO;
 import com.beuben.pokemontcgcollectorbackend.refresh.infrastructure.out.external.mapper.CardMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static com.beuben.pokemontcgcollectorbackend.refresh.infrastructure.out.external.api.pokemontcg.PokemonTcgApiParam.INITIAL_PAGE;
 
 @Component
 @RequiredArgsConstructor
@@ -16,7 +20,25 @@ public class CardAdapter implements CardProvider {
 
   @Override
   public Flux<Card> findAll() {
-    return pokemonTcgApi.findAll()
+    return fetchAllPages()
+        .flatMapIterable(PokemonTcgCardWrapperDTO::data)
         .map(mapper::toDomain);
+  }
+
+  private Flux<PokemonTcgCardWrapperDTO> fetchAllPages() {
+    return pokemonTcgApi.findAll(INITIAL_PAGE)
+        .expand(responseWrapper -> {
+          var totalCount = responseWrapper.totalCount();
+          var pageSize = responseWrapper.pageSize();
+          var totalPages = (int) Math.ceil((double) totalCount / pageSize);
+
+          var currentPage = responseWrapper.page();
+
+          if (currentPage < 2) {
+            return pokemonTcgApi.findAll(currentPage + 1);
+          } else {
+            return Mono.empty();
+          }
+        });
   }
 }
