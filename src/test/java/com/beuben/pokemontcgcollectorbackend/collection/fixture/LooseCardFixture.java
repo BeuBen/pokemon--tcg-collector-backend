852b32239bc9ccf8b072f861507925d999642921
Benjamin Beuscart <beuscart.ben@gmail.com>
2025-02-16 20:00:44 +0100

    test(collection): loose card mapper tests

--- src/test/java/com/beuben/pokemontcgcollectorbackend/collection/fixture/LooseCardFixture.java
+++ src/test/java/com/beuben/pokemontcgcollectorbackend/collection/fixture/LooseCardFixture.java
@@ -3,6 +3,8 @@ package com.beuben.pokemontcgcollectorbackend.collection.fixture;
 import com.beuben.pokemontcgcollectorbackend.collection.domain.Condition;
 import com.beuben.pokemontcgcollectorbackend.collection.domain.Language;
 import com.beuben.pokemontcgcollectorbackend.collection.domain.LooseCard;
+import com.beuben.pokemontcgcollectorbackend.collection.infrastructure.in.rest.dto.command.AddLooseCardCommand;
+import com.beuben.pokemontcgcollectorbackend.collection.infrastructure.in.rest.dto.result.LooseCardDTO;
 import com.beuben.pokemontcgcollectorbackend.collection.infrastructure.out.persistence.entity.LooseCardEntity;
 
 import java.math.BigDecimal;
@@ -41,5 +43,37 @@ public class LooseCardFixture {
         .withPicturesUrl("pictures_url.com")
         .withComment("a comment")
         .withCreationDate(LocalDateTime.MIN);
+  }
+
+  public static LooseCardDTO aValidLooseCardDTO() {
+    return LooseCardDTO.builder()
+        .id(1L)
+        .collectorId(1L)
+        .label("test")
+        .cardId(1L)
+        .language(Language.FRENCH)
+        .firstEdition(false)
+        .reverseHolo(false)
+        .condition(Condition.MINT)
+        .estimation(ItemFixture.aValidEstimation())
+        .picturesUrl("pictures_url.com")
+        .comment("a comment")
+        .creationDate(LocalDateTime.MIN)
+        .build();
+  }
+
+  public static AddLooseCardCommand aValidAddLooseCardCommand() {
+    return AddLooseCardCommand.builder()
+        .label("test")
+        .cardId(1L)
+        .language(Language.FRENCH)
+        .firstEdition(false)
+        .reverseHolo(false)
+        .condition(Condition.MINT)
+        .estimationEuros(BigDecimal.TEN)
+        .estimationSourceUrl("estimation_url.com")
+        .picturesUrl("pictures_url.com")
+        .comment("a comment")
+        .build();
   }
 }
---
+++ src/test/java/com/beuben/pokemontcgcollectorbackend/collection/infrastructure/in/rest/mapper/LooseCardMapperTest.java
@@ -0,0 +1,49 @@
+package com.beuben.pokemontcgcollectorbackend.collection.infrastructure.in.rest.mapper;
+
+import com.beuben.pokemontcgcollectorbackend.collection.fixture.ItemFixture;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.beuben.pokemontcgcollectorbackend.collection.fixture.CollectorFixture.aValidCollector;
+import static com.beuben.pokemontcgcollectorbackend.collection.fixture.LooseCardFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class LooseCardMapperTest {
+
+  @Autowired
+  private LooseCardMapper mapper;
+
+  @Test
+  void add_loose_card_command_mapping_should_return_the_right_loose_card() {
+    // ARRANGE
+    final var collectorId = aValidCollector().getId();
+    final var command = aValidAddLooseCardCommand();
+    final var expected =
+        aValidLooseCard()
+            .withId(null)
+            .withEstimation(ItemFixture.aValidEstimation()
+                .withDate(null))
+            .withCreationDate(null);
+
+    // ACT
+    final var mapped = mapper.toDomain(command, collectorId);
+
+    // ASSERT
+    assertThat(mapped).isEqualTo(expected);
+  }
+
+  @Test
+  void loose_card_mapping_should_return_the_right_loose_card_dto() {
+    // ARRANGE
+    final var model = aValidLooseCard();
+    final var expected = aValidLooseCardDTO();
+
+    // ACT
+    final var mapped = mapper.toDTO(model);
+
+    // ASSERT
+    assertThat(mapped).isEqualTo(expected);
+  }
+}
